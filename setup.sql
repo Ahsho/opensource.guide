2fdeea646d78cc97ab59a2caaa7183bec06b6465
Ahsho <86135689+Ahsho@users.noreply.github.com>
2022-07-14 03:06:12 +0800

    docker run -d -p 5432:5432 --name postgis-db -e POSTGRES_PASSWORD=root -d mdillon/postgis docker exec -it postgis-db sh -c 'exec psql -U postgres'

---
+++ setup.sql
@@ -0,0 +1,33 @@
+/*
+Andrew Henry
+12/17/2018
+Yam Database
+*/
+
+-- First need to delete everything
+DROP SCHEMA yamschema
+CASCADE;
+CREATE SCHEMA yamschema;
+
+SET
+SCHEMA 'yamschema';
+
+-- Extensions go here
+CREATE EXTENSION postgis;
+CREATE EXTENSION citext;
+
+BEGIN;
+    \i setup-files/tables.sql
+\i setup-files/functions.sql
+\i setup-files/updates.sql
+\setup-files/datainsert.sql
+    COMMIT;
+
+    -- permissions have be last, as there is nothing created before
+    GRANT USAGE ON SCHEMA yamschema TO yamuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA yamschema TO yamuser;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA yamschema TO yamuser;
+ALTER DATABASE yamdb SET search_path
+TO yamschema;
+
+
